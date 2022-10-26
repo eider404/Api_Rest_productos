342f269cd6d57d2fcb33cbd807681d46523c3bf1
Eider Pool <eider.pool.arjona@gmail.com>
2022-10-25 20:38:23 -0500

    views agregadas, pero aun no funciona el put y delete correctamente en routes.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port= 3000;
 //impotar rutas
 const routes = require("./routes")
 
+const bodyParcer = require('body-parser');
+
 //coneccion a la DB
 const mysql = require('mysql');
 const myconnection = require("express-myconnection");
@@ -23,12 +25,14 @@ try {
 }
 
 //analiza las solicitudes JSON 
-app.use(express.json())
-
+//app.use(express.json())
+app.use(bodyParcer.json());
+app.use(bodyParcer.urlencoded({extended:false}));
 
 //set the view engine to ejs
-//app.set('views','./views')
-//app.set('view engine', 'ejs');
+app.set('views','./views')
+app.set('view engine', 'ejs');
+
 //direcciones
 app.use("/",routes) 
 
--- routes.js
+++ routes.js
@@ -1,22 +1,6 @@
-const { json } = require("express");
+const { json, application } = require("express");
 const express = require("express")
 const routes = express.Router()
-
-routes.get('/:id',(req, res)=>{
-    //res.render('pages/index');
-    
-    req.getConnection((err, conn)=>{
-        if(err) {
-            return res.send(err) 
-        }
-        
-        conn.query("SELECT * FROM Product WHERE id = ?", [req.params.id], (err, rows)=>{
-            if(err) { return res.send(err) }
-            //res.render('pages/index',{rows});
-            res.json(rows);
-        })    
-    })
-})
 
 
 routes.get('/',(req, res)=>{
@@ -26,54 +10,81 @@ routes.get('/',(req, res)=>{
         
         conn.query("SELECT * FROM Product", (err, rows)=>{
             if(err) { return res.send(err) }
-            //res.render('pages/index',{rows});
-            res.json(rows);
+            //res.render('index',{rows});
+            //res.json(rows); 
+            res.render('index',{rows})
         })   
     })
 })
 
+routes.get('/new',(req,res)=>{
+    res.render('new',{title: 'Nueva entrada'})
+})
+routes.get('/update',(req,res)=>{
+    res.render('update',{title: 'actualizar entrada'})
+})
+routes.get('/delete',(req,res)=>{
+    res.render('delete',{title: 'eliminar entrada'})
+})
 
 
-
-routes.post('/',(req, res)=>{
+routes.post('/new',(req, res)=>{
     
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err)}
          
         conn.query("INSERT INTO Product set ?", [req.body], (err, rows)=>{
             if(err) { return res.send(err) }
-            res.send(`product inserted `);
+            //res.send(`product inserted `);
+            res.redirect('/')
         })
         
     })
 })
 
 
-routes.put('/:id',(req, res)=>{
+//put FUNCIONA pero no como deberia ser
+routes.post('/update',(req, res)=>{
     
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err) }
           
-        conn.query("UPDATE Product set ? WHERE id = ?",[req.body, req.params.id], (err, rows)=>{
+        conn.query("UPDATE Product set ? WHERE id = ?",[req.body, /*req.params.id*/req.body.id], (err, rows)=>{
              if(err) { return res.send(err) }
-             res.send(`product update`);
+             //res.send(`product update`);
+             res.redirect('/')
         })
-    })
+    }) 
+    
 })
 
-routes.delete('/:id',(req, res)=>{
+//delete FUNCIONA pero no como deberia ser
+routes.post('/delete',(req, res)=>{
     
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err) }
           
-        conn.query("DELETE FROM Product WHERE id = ?",[req.params.id], (err, rows)=>{
+        conn.query("DELETE FROM Product WHERE id = ?",[/*req.params.id*/req.body.id ], (err, rows)=>{
              if(err) { return res.send(err) }
-             res.send(`product deleted `);
+             //res.send(`product deleted `);
+             res.redirect('/')
         })
     })
 })
 
-
+routes.get('/:id',(req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) {
+            return res.send(err) 
+        }
+        
+        conn.query("SELECT * FROM Product WHERE id = ?", [req.params.id], (err, rows)=>{
+            if(err) { return res.send(err) }
+            //res.status(404).send('Sorry cant find that!');
+            res.json(rows);
+        })    
+    })
+})
 
 
 
